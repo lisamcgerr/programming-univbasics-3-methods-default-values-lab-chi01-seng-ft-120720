64559521e87ccd196a1f567ecb838b719533ce67
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-11 15:48:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
-def meal_choice(veg1, beg2, protein = todu) 
+def meal_choice(veg1, veg2, protein = todu) 
  puts "What a nutritious meal!"
  puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
